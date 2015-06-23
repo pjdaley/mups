389319e12a717ea1ce063a0b17ce1d7ea99a4b6d
Patrick Daley <pjdaley95@gmail.com>
2015-06-22 23:25:26 -0400

    Added functionality to home boxs and drop downs to the panels in programs section.

--- custom.js
+++ custom.js
@@ -1,3 +1,19 @@
-function changeColor() {
-	document.getElementById('eventbox').style.bordercolor:black;
-}
+$fadetime = 150;
+$slidetime = 300;
+
+$(document).ready(function(){
+	$(".inner,.panel-body").hide();
+    $(".homebox").mouseenter(function(){
+        $(this).addClass("borders");
+        $(this).children("h2,img").fadeToggle($fadetime);
+        $(this).children(".inner").delay($fadetime).fadeToggle($fadetime);
+    });
+    $(".homebox").mouseleave(function(){
+        $(this).removeClass("borders");
+        $(this).children("h2,img").delay($fadetime).fadeToggle($fadetime);
+        $(this).children(".inner").fadeToggle($fadetime);
+    });
+    $(".panel").click(function(){
+    	$(this).children(".panel-body").slideToggle($slidetime);
+    });
+});
