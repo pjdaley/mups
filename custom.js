5c433c227153dfba90636ec95944977782cb88e2
Patrick Daley <pjdaley95@gmail.com>
2015-07-12 23:30:46 -0400

    added phi page

--- custom.js
+++ custom.js
@@ -1,8 +1,8 @@
-$fadetime = 150;
+$fadetime = 100;
 $slidetime = 300;
 
 $(document).ready(function(){
-	$(".inner,.panel-body").hide();
+	$(".inner,.panel-body,.glyphicon-chevron-down").hide();
     $(".homebox").mouseenter(function(){
         $(this).addClass("borders");
         $(this).children("h2,img").fadeToggle($fadetime);
@@ -13,7 +13,30 @@ $(document).ready(function(){
         $(this).children("h2,img").delay($fadetime).fadeToggle($fadetime);
         $(this).children(".inner").fadeToggle($fadetime);
     });
+  	$(".panel").mouseenter(function(){
+  		$(this).find(".glyphicon-chevron-down").show();
+  	});
+  	$(".panel").mouseleave(function(){
+  		$(this).find(".glyphicon-chevron-down").hide();
+  	});
     $(".panel").click(function(){
     	$(this).children(".panel-body").slideToggle($slidetime);
     });
+    $(".contactbox").mouseleave(function(){
+    	$(this).addClass("light-borders");
+    	$(this).removeClass("purple-box");
+    });
+    $(".contactbox").mouseenter(function(){
+    	$(this).removeClass("light-borders");
+    	$(this).addClass("purple-box");
+    });
 });
+
+/*double link javascript
+document.getElementById("department").onclick = function() {
+    window.open("http://www.someothersite.com/");
+}*/
+
+// twiter button script
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
